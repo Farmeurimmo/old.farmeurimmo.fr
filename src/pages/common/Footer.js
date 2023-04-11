1c0ef8000d855e565770284bb700814d289e60cd
Farmeurimmo <farmeurimmo@gmail.com>
2023-04-11 20:17:48 +0200

    Fix footer + placeholder + couleur message d'erreur

--- src/pages/common/Footer.js
+++ src/pages/common/Footer.js
@@ -5,7 +5,6 @@ function Footer() {
         <footer>
             <p className="links">
                 <a className="link" href="https://farmeurimmo.fr">Farmeurimmo &copy; 2018 - 2023</a>
-                <a className="link" href="https://github.com/Farmeurimmo/farmeurimmo.fr">Github</a>
                 <a className="link" href="https://fr.reactjs.org">Technologie utilisée</a>
                 <a className="link" href="https://pages.dev">Hébergeur</a>
                 <a className="link" href="https://cloudflare.com">Protections</a>
