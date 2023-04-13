88ea4ea4da8770e61640415ebba5fee6eab5f8d3
Farmeurimmo <farmeurimmo@gmail.com>
2023-04-13 16:37:21 +0200

    Code refacto

--- src/pages/accueil/Presentation.js
+++ src/pages/accueil/Presentation.js
@@ -8,7 +8,9 @@ function Presentation() {
                 <div className="present">
                     <h1 className="pseudo">Farmeurimmo</h1>
                     <ul className="list">
-                        <li className="ele">Développeur Java, Web (HTML,CSS,JS/TS), C++, SQL, Python, API Mc: Spigot, Bungee & Velocity</li>
+                        <li className="ele">Développeur Java, Web (HTML,CSS,JS/TS), C++, SQL, Python, API Mc: Spigot,
+                            Bungee & Velocity
+                        </li>
                         <li className="ele">Administrateur Système</li>
                         <li className="ele">Amateur de cybersécurité & d'IA</li>
                     </ul>
