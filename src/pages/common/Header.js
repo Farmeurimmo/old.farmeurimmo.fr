c9d778bd51ecefa988cdf04623f553be2f2bc658
Farmeurimmo <farmeurimmo@gmail.com>
2023-04-13 10:25:05 +0200

    Ajout d'animation pour les liens

--- src/pages/common/Header.js
+++ src/pages/common/Header.js
@@ -7,11 +7,11 @@ function App() {
         <header>
             <nav>
                 <ul>
-                    <li><a href="/">Accueil</a></li>
-                    <li><a href="/dev">Développement</a></li>
-                    <li><a href="/as">Administration Système</a></li>
-                    <li><a href="/projets">Mes projets</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><a className="animated" href="/">Accueil</a></li>
+                    <li><a className="animated" href="/dev">Développement</a></li>
+                    <li><a className="animated" href="/as">Administration Système</a></li>
+                    <li><a className="animated" href="/projets">Mes projets</a></li>
+                    <li><a className="animated" href="/contact">Contact</a></li>
                 </ul>
             </nav>
         </header>
